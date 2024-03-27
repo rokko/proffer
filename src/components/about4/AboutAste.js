034ec2b551462c69dd1def06901d5e38254e062e
Rocco Caricola <rocco.caricola@alten.it>
2024-03-27 23:07:23 +0100

    aste

---
+++ src/components/about4/AboutAste.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const AboutAste= (props) => {
+
+    return (
+        <section className="about-page-section section-padding">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col col-lg-8 col-md-10">
+                        <div className="section-title-s3">
+                            <span>#Aste</span>
+                            <h2>As<span>te </span>.</h2>
+                            <p>Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <p>Descrizione DescrizioneDescrizioneDescrizioneDescrizioneDescrizioneDescrizione</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AboutAste;
--- src/main-component/AboutPage/Aste.js
+++ src/main-component/AboutPage/Aste.js
@@ -15,15 +15,14 @@ import Instagram from '../../components/Instagram/Instagram';
 import Footer from '../../components/footer/Footer';
 import Logo from '../../images/logo.png'
 import MissionSection2 from '../../components/MissionSection/MissionSection2';
+import AboutAste from '../../components/about4/AboutAste';
 
 const Aste= () => {
     return (
         <Fragment>
             <Navbar hclass={'header-style-1'} Logo={Logo} btnClass={'theme-btn-s1'} />
             <PageTitle pageTitle={'Aste'} pagesub={'Aste'} />
-            <About4 />
-            <MissionSection2 />
-           
+            <AboutAste /> 
             <Footer />
             <Scrollbar />
         </Fragment>
