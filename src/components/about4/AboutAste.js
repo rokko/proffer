860b1473dc3383b02876cf921815fc2dfee35552
Rocco Caricola <roccocaricola.dev@outlook.com>
2024-05-30 18:13:12 +0200

    inserimento cursore canestro

--- src/components/about4/AboutAste.js
+++ src/components/about4/AboutAste.js
@@ -10,7 +10,14 @@ const AboutAste= (props) => {
                         <div className="section-title-s3">
                             <span>#Aste</span>
                             <h2>As<span>te </span>.</h2>
-                            <p>Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+                            <p>LValerio, oltre ad essere un grande appassionato di basket, ha collezionato nel corso degli anni una vasta gamma di cimeli sportivi, tra cui canotte, palloni autografati e altri memorabilia unici. Partecipare alle nostre aste significa non solo avere l'opportunità di possedere un pezzo di storia sportiva, ma anche contribuire direttamente ai nostri progetti.
+
+Le aste dei cimeli sportivi di Valerio sono organizzate regolarmente e aperte a tutti. Ogni oggetto messo all’asta è accompagnato da una descrizione dettagliata della sua storia.
+
+I fondi raccolti vengono interamente destinati alle nostre iniziative, permettendoci di costruire nuovi impianti sportivi e offrire supporto educativo ai giovani in difficoltà.
+
+Partecipando alle nostre aste, non solo potrai aggiungere un prezioso cimelio alla tua collezione, ma anche fare la differenza nella vita di molti ragazzi. Resta aggiornato sulle prossime aste iscrivendoti alla nostra newsletter e seguendo i nostri canali social.
+</p>
                         </div>
                     </div>
                 </div>
