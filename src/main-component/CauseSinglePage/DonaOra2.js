63b6cefc7f12a1edfd3b0475043c5328c4514233
Rocco Caricola <rocco.caricola89@gmail.com>
2024-07-05 00:39:00 +0200

    Dona ora correzione

--- src/main-component/CauseSinglePage/DonaOra2.js
+++ src/main-component/CauseSinglePage/DonaOra2.js
@@ -3,6 +3,7 @@ import './donaora.style.scss';
 import Navbar from '../../components/Navbar/Navbar';
 import Logo from '../../images/logo.png'
 import Footer from '../../components/footer/Footer';
+import FormDonazioni from '../../components/Targetarea/FormDonazioni';
 
 function DonaOra2() {
   const [formData, setFormData] = useState({
@@ -49,89 +50,8 @@ Quest’ultima è veramente speciale: Roma non è nata in un giorno, e poter con
 
 Unisciti a noi nella missione di trasformare la passione per il basket in uno strumento di cambiamento sociale.<br/> Dona ora e aiuta a costruire un futuro migliore per tanti giovani.<br/>
 </p>
-      <br/>
-      <br/> <br/>
-      <br/>
-    <form className="form-dona" onSubmit={handleSubmit}>
-      <div className="input-group">
-        <input
-          type="text"
-          name="nome"
-          value={formData.nome}
-          onChange={handleChange}
-          placeholder="Nome"
-        />
-        <input
-          type="text"
-          name="cognome"
-          value={formData.cognome}
-          onChange={handleChange}
-          placeholder="Cognome"
-        />
-      </div>
-      <div className="input-group">
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="Email"
-        />
-        <input
-          type="text"
-          name="cellulare"
-          value={formData.cellulare}
-          onChange={handleChange}
-          placeholder="Cellulare"
-        />
-      </div>
-      <div className="input-group">
-        <input
-          type="text"
-          name="codiceFiscale"
-          value={formData.codiceFiscale}
-          onChange={handleChange}
-          placeholder="Codice Fiscale"
-        />
-      </div>
-      <div className="input-group">
-        <select name="tipoPagamento" value={formData.tipoPagamento} onChange={handleChange}>
-          <option value="singola">Singola</option>
-          <option value="periodica">Periodica Mensile</option>
-        </select>
-        <input
-          type="number"
-          name="importo"
-          value={formData.importo}
-          onChange={handleChange}
-          placeholder="Importo"
-        />
-      </div>
-      <div className="input-group">
-        <input
-          type="text"
-          name="numeroCarta"
-          value={formData.numeroCarta}
-          onChange={handleChange}
-          placeholder="Numero Carta"
-        />
-        <input
-          type="text"
-          name="scadenzaCarta"
-          value={formData.scadenzaCarta}
-          onChange={handleChange}
-          placeholder="Scadenza MM/AA"
-        />
-        <input
-          type="text"
-          name="cvv"
-          value={formData.cvv}
-          onChange={handleChange}
-          placeholder="CVV"
-        />
-      </div>
-      <button type="submit">Invia</button>
-    </form>
+    
+    <FormDonazioni />
     
 
     </div>
