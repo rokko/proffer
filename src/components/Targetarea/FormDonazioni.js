c9a9393b3e25c53b67031757ff22e540c10483f4
Rocco Caricola <rocco.caricola89@gmail.com>
2024-07-07 18:23:29 +0200

    correzione spazio form donazioni

--- src/components/Targetarea/FormDonazioni.js
+++ src/components/Targetarea/FormDonazioni.js
@@ -11,7 +11,7 @@ const FormDonazioni = (props) => {
 
     return (
      
-                <div className="row">
+                <div className="row" style={{marginBottom:'-20px'}}>
                     
               
                    
