281b8c36513e39b92ff8b63db51b813a02ae37ad
Rocco Caricola <roccocaricola.dev@outlook.com>
2024-11-09 16:15:37 +0100

    dashborad prima versione

--- src/main-component/DashboardValePerTutti.js
+++ src/main-component/DashboardValePerTutti.js
@@ -2,12 +2,15 @@ import react, {useState} from 'react'
 
 const DashboardValePerTutti = () => {
 
-    const salva = () =>{
+    const salvainput = () =>{
         console.log(titolo)
         console.log(testo)
     }
     const [titolo, setTitolo] = useState()
     const [testo, setTesto] = useState()
+    const [immagine1, setImmagine1] = useState()
+    const [immagine2, setImmagine2] = useState()
+    const [NomeAccorciato, setNomeAccorciato] = useState()
     return(
         <div>
         <p>Dashboard</p>
@@ -15,14 +18,17 @@ const DashboardValePerTutti = () => {
         <div style={{display:'flex', flexDirection:'column'}}>
             <p>Titolo</p>
         <input id="titolo"  onChange={(e)=> setTitolo(e.target.value)}></input>
+        <p>NomeAccorciato</p>
+        <input id="titolo"  onChange={(e)=> setNomeAccorciato(e.target.value)}></input>
         <p>Testo</p>
-        <input id="testo" onChange={(e)=> setTesto(e.target.value)}></input>
-        <button onClick={()=>salva()}></button>
+        <textarea id="testo"  rows="5"
+        cols="50" onChange={(e)=> setTesto(e.target.value)}></textarea>
+       
         <p>Immagine1</p>
         <input id="url1" onChange={(e)=> setImmagine1(e.target.value)}></input>
         <p>Immagine 2 opzionale</p>
         <input id="url2" onChange={(e)=> setImmagine2(e.target.value)}></input>
-
+        <button onClick={()=>salvainput()}>INVIA</button>
         </div>
         </div>
     )
