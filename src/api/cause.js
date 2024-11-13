92fe9338815f893b7b885c83c7276a1e443974cc
Rocco Caricola <roccocaricola.dev@outlook.com>
2024-11-13 18:14:58 +0100

    foto kalifa

--- src/api/cause.js
+++ src/api/cause.js
@@ -1,7 +1,7 @@
 import sveva from './imageCauses/IMG_8315.jpg'
 import camerunCourt from './imageCauses/Card Articolo Camerun Court NUOVA.webp'
 import khalifa from './imageCauses/cardarticolokalifa.webp'
-import fotoArticoloKhalifa from './imageCauses/Foto Articolo Khalifa.webp'
+import fotoArticoloKhalifa from './imageCauses/fotoarticolokalifa.webp'
 
 import fotoArticoloCamerunCourt from './imageCauses/Foto Articolo Camerun Court (4).webp'
 import fotoArticoloCamerunCourt2 from './imageCauses/Foto Articolo Camerun Court (5).webp'
@@ -48,7 +48,8 @@ const Causes = [
         Goal:'3,000.00',
         Raised:'2,000.00',
         authorName:'Vale Per tutti',
-        fotoArticolo :fotoArticoloKhalifa,
+        fotoArticolo :khalifa,
+        fotoArticolo2: fotoArticoloKhalifa,
         testo : 'Nasce tutto da un messaggio social ricevuto sulla pagina di Vale per tutti.\nKhalifa, un ragazzo di 17 anni nigeriano, ci chiede di aiutarlo a realizzare il suo più grande sogno: diventare un giocatore di basket professionista.\nDa qui nasce la voglia di riuscire ad arrivare anche ai ragazzi che non vengono raggiunti dai nostri progetti direttamente e che si trovano in zone diverse da quelle dove abbiamo operato finora.\nVorremmo che TUTTI avessero la possibilità di coltivare la propria passione per la pallacanestro, ma allo stesso tempo, non possiamo essere ovunque.\nKhalifa con il suo amore per la pallacanestro, ci fornisce l’assist per uno dei nostri progetti che chiameremo “Feel the Dream”.\nAiutare ragazzi o ragazze che non hanno le possibilità di giocare a pallacanestro a farlo.\nAbbiamo così scelto di dare una mano a Khalifa a distanza spedendogli (con tutte le difficoltà del caso) abbigliamento ed attrezzature sportive che potessero accompagnarlo nei suoi allenamenti.\nA distanza di tempo siamo ancora in contatto con lui, continueremo ad aiutarlo a rincorrere il suo sogno e chi lo sa, magari un giorno riusciremo anche ad aiutarlo con la sua carriera o ad incontrarlo in uno dei nostri viaggi in Africa.\nFacciamo tutti il tifo per te e invitiamo chiunque abbia le stesse necessità, in qualsiasi parte del mondo, a chiedere il nostro aiuto!',
     },
     {
