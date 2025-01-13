798d954176bb32986988f0c15d75dfa874a50a04
Rocco Caricola <rocco.caricola89@gmail.com>
2025-01-13 16:11:52 +0100

    altre fix sottoineature

--- src/components/MissionSection/MissionSectionStoria.js
+++ src/components/MissionSection/MissionSectionStoria.js
@@ -11,15 +11,15 @@ const MissionSectionStoria = (props) => {
                             <div className="grid" style={{padding:'20px', fontSize:'25px', borderRadius:'20px'}}>
                                 <div className="overlay"></div>
                                
-                                <p>Il 3 giugno 2022 viene inaugurato il primo campo dedicato a Valerio.
-In Kenya, a Korogocho, una delle tante baraccopoli di Nairobi, che sorge vicino alla grande discarica a cielo aperto di Dandora.
+                                <p>Il<strong> 3 giugno 2022</strong> viene inaugurato il <strong>primo campo</strong> dedicato a Valerio.
+In <strong>Kenya</strong>, a Korogocho, una delle tante baraccopoli di Nairobi, che sorge vicino alla grande discarica a cielo aperto di Dandora.
 Sono 7 giorni di fatica e scoperta, che ci cambiano profondamente e piantano nei nostri cuori, il seme di un’idea.
 </p>
                             </div>
                             <div className="grid" style={{padding:'20px', fontSize:'25px', borderRadius:'20px', display:'none'}}>
                                 <div className="overlay"></div>
                                
-                                <p >5 luglio: è passato appena un mese, e passiamo una giornata intera a sognare come restituire l’amore per la pallacanestro che Vale ci ha trasmesso. Sogniamo altri campi da basket, sogniamo clinic di formazione in mezzo alle baraccopoli…non ci poniamo limiti. E così, tra lacrime e risate prende forza l’idea di un’associazione che porti avanti i sogni di Valerio. Non ci sono ancora nome, logo, statuto, bilanci e la strada sembra lunghissima…ma sinceramente a noi, non importa.
+                                <p ><strong>5 luglio</strong>: è passato appena un mese, e passiamo una giornata intera a sognare come restituire l’amore per la pallacanestro che Vale ci ha trasmesso. <strong>Sogniamo</strong> altri campi da basket, sogniamo clinic di formazione in mezzo alle baraccopoli…non ci poniamo limiti. E così, tra lacrime e risate prende forza <strong>l’idea</strong> di un’associazione che porti avanti i sogni di Valerio. Non ci sono ancora nome, logo, statuto, bilanci e la strada sembra lunghissima…ma sinceramente a noi, non importa.
 </p>
                             </div>
                             <div className="grid" style={{padding:'20px', fontSize:'25px', display:'', borderRadius:'20px'}}>
@@ -30,8 +30,8 @@ Sono 7 giorni di fatica e scoperta, che ci cambiano profondamente e piantano nei
                             </div>
                             <div className="grid" style={{padding:'20px', fontSize:'25px', borderRadius:'20px'}}>
                                 <div className="overlay"></div>
-                                <p>Luglio e agosto sono mesi di fuoco, in tutti i sensi.
-Ma la realtà è che molteplici tasselli vanno ad incastrarsi perfettamente l’uno con l’altro come fossero un puzzle, o forse come se qualcuno dall’alto ci desse una mano…chissà, sta di fatto che il 20 settembre 2022 nasce ufficialmente Vale Per Tutti con l’inaugurazione del murales ufficiale della NBA, dove sono ritratti due grandi campioni del basket, insieme a Belinelli e Ginobili.
+                                <p>Luglio e agosto sono <strong> mesi di fuoco</strong>, in tutti i sensi.
+Ma la realtà è che molteplici tasselli vanno ad incastrarsi perfettamente l’uno con l’altro come fossero un <strong>puzzle</strong> , o forse come se qualcuno dall’alto ci desse una mano…chissà, sta di fatto che il <strong>20 settembre 2022</strong> nasce ufficialmente Vale Per Tutti con l’inaugurazione del <strong>murales</strong> ufficiale della NBA, dove sono ritratti due grandi campioni del basket, insieme a Belinelli e Ginobili.
 </p>
                             </div>
                         </div>
--- src/components/about4/missionText.js
+++ src/components/about4/missionText.js
@@ -11,18 +11,18 @@ const MissionText = (props) => {
                         <div className="section-title-s3">
                         <h2>
                 Mission
-              </h2>                            <p>Proprio come Valerio ha fatto con noi, la nostra mission è quella di regalare l’amore per il basket e per lo sport in generale, a tutte quelle persone che hanno bisogno di sognare e di porsi degli obiettivi, nella speranza di cambiare in meglio le loro vite.
+              </h2>                            <p>Proprio come Valerio ha fatto con noi, la nostra <strong> mission</strong> è quella di <strong>regalare l’amore per il basket e per lo sport</strong> in generale, a tutte quelle persone che hanno bisogno di sognare e di porsi degli obiettivi, nella speranza di cambiare in meglio le loro vite.
 
                             </p>
-                            <p>Per farlo, partiamo dalla riqualificazione e realizzazione di spazi destinati allo sport per bambini e ragazzi che vivono in situazioni difficili e sono meno fortunati di noi.
+                            <p>Per farlo, partiamo dalla <strong>riqualificazione e realizzazione</strong> di spazi destinati allo sport per bambini e ragazzi che vivono in situazioni difficili e sono meno fortunati di noi.
 </p>
 <img src={mission1} alt='mission1' />
-                            <p>Il secondo passo è offrire corsi di formazione per il personale locale, così da mettere a disposizione tutti gli ingredienti necessari per la nascita di nuove realtà sportive.
+                            <p>Il secondo passo è offrire <strong> corsi di formazione</strong> per il personale locale, così da mettere a disposizione tutti gli ingredienti necessari per la nascita di nuove realtà sportive.
                             </p>
-                            <p>Le nostre iniziative si svilupperanno in Italia, in Africa o in qualsiasi altra parte del mondo. Tutto ciò è e sarà possibile solo grazie alle donazioni di chi sceglierà di sostenerci.
+                            <p>Le nostre iniziative si svilupperanno in Italia, in Africa o in qualsiasi altra parte del mondo. Tutto ciò è e <strong>sarà possibile</strong> solo grazie alle <strong>donazioni</strong> di chi sceglierà di sostenerci.
 
                             </p>
-                            <p>I nostri progetti saranno dinamici, ambiziosi e senza confini, un po' come Valerio stesso.
+                            <p>I nostri progetti saranno dinamici, ambiziosi e <strong>senza confini</strong>, un po' come Valerio stesso.
 
                             </p>
                             <img src={mission2} alt='mission2' />
