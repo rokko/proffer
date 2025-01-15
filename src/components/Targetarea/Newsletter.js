5046a86c41188320a369cb5b7fef3f8ae9ef8645
Rocco Caricola <rocco.caricola89@gmail.com>
2025-01-15 21:25:18 +0100

    add usestate

--- src/components/Targetarea/Newsletter.js
+++ src/components/Targetarea/Newsletter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import abimg from '../../images/target.jpg'
 import { Link } from 'react-router-dom';
 import './newsletter.style.scss'
