cfed243a9287529b6900186c8575d006d6df4fd5
Rocco Caricola <rocco.caricola89@gmail.com>
2025-02-26 18:06:57 +0100

    correzione newsletter

--- src/components/Targetarea/Newsletter.js
+++ src/components/Targetarea/Newsletter.js
@@ -6,6 +6,7 @@ import { collection, addDoc } from "firebase/firestore";
 import { db } from "../../main-component/firebase"
 const NewsLetter = (props) => {
     const [emailNewsletter, setEmailNewsletter]=useState('')
+    const [inviato, setInviato] = useState(false)
 
     const ClickHandler = () => {
         window.scrollTo(10, 0);
@@ -15,6 +16,7 @@ const NewsLetter = (props) => {
         e.preventDefault();
         try{ 
             await addDoc(collection(db, "users"), {email:emailNewsletter})
+            setInviato(true)
         }
         catch(err){
             console.error(err)
@@ -40,7 +42,10 @@ const NewsLetter = (props) => {
                             <form action="" className='form-css'>
                                 <input type="email" name="email" id="email" placeholder="email@email.it" onChange={(e)=>setEmailNewsletter(e.target.value)} />
                                 <input style={{ backgroundColor: '#78c3e0' }} type="submit" name="submit" value="Iscriviti" onClick={(e)=>saveEmail(e)} />
+
                             </form>
+                            {inviato ? (<div style={{marginTop:'10px',display: 'flex', flexDirection: 'row'}}><img style={{width:'32px' , height:'32px'}} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAABN0lEQVR4nO2YTUsDMRCGc/YDFcSbIP4MEQKZduZg8P8V1AmVXvwRtaLg11W8ir9EWWFx3d2WblubBN4HctnD5nlnwmZYYwAAAAAAQJL0h/1NUr62KkcmN/zAb5DKmIJ8OZVPuqRjk1PlnfKkkP9d/MFXfGhyqjxVllO+tzd2y6SMh3wkPCofCY/KRwKVjwUq34Vi7qDAw2IuMSuZKuWuZTyYrOL9bRseOJX3cgY5uTjfzubY2JHfJ+W3+maLhIhy5k9HZ3tO5bVt0y4hZskv09G1hIgqX2LV7rrALw2JIA+zJJKQr4Ygled5QyQl3zVEkvIlvUFvh1SepoVIWr7WiUaInwuq5ZIqnv3LJbVsJ1zgx4ZsypWf75dHJvJ/QgS+zVJ+Wois5GtT5jhL+WqI5L42AABg1sU3ZcAexnacT8QAAAAASUVORK5CYII=" alt="checkmark--v1"></img><p>Registrazione effettuata</p></div>):''}
+
                         </div>                
                         </div>                
                         </div>
